7e12a3f5020e5ac09fec8aad7c731b498fa7df91
shihab-shamim <shihabshamim767@gmail.com>
2025-07-08 16:17:09 +0600

    acticeCards ar css added

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '59e6baac7c43fe1586a5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '9b0d7d60deb1b99e225d');
